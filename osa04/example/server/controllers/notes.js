ad9d5c7294e9fb94710699bf84803998a0e00007
reijjo <teemu.aitomeri@gmail.com>
2023-10-17 11:31:28 +0300

    4c done

--- osa04/example/server/controllers/notes.js
+++ osa04/example/server/controllers/notes.js
@@ -1,22 +1,33 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
+
+// api/notes
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
+
+// api/notes/:id
 
 notesRouter.get("/:id", async (request, response, next) => {
   const note = await Note.findById(request.params.id);
