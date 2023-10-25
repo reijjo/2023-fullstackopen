cdeef38065fff70c7e491d4bb8a87d4dab8d41e9
reijjo <teemu.aitomeri@gmail.com>
2023-10-25 17:15:04 +0300

    5b done

--- osa05/example/client/src/components/Togglable.jsx
+++ osa05/example/client/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, useImperativeHandle, forwardRef } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
@@ -30,3 +31,7 @@ const Togglable = forwardRef((props, ref) => {
 });
 
 export default Togglable;
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
---
+++ osa05/readme.md
@@ -0,0 +1,3 @@
+# 5.12
+
+Kaytan Prettier VSCodessa niin en halua linttia sinne sotketaan turhasta. Siksi jatin tekematta.
